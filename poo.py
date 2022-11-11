1bc79339113b7ccd889e2d9fc98bde5167248717
Rafael Flores <r.flores@levelup.gt>
2022-11-10 19:55:29 -0600

    Add exercises about poo properties

--- poo.py
+++ poo.py
@@ -27,3 +27,81 @@ billy = Member('Billy')
 #billy.name = 'Billy'
 billy.welcome()
 
+# Herencia
+class Person():
+    first_name = None
+    last_name = None
+    birthdate = None
+
+    def be_in_gym(self):
+        print('Estoy en el gimnasio')
+
+class Trainer(Person):
+    salary = None
+
+    def be_in_gym(self):
+        print('Estoy trabajando')
+
+class Member(Person):
+    imc = None
+
+    def be_in_gym(self):
+        print('Estoy ejercitandome')
+
+alumno = Member()
+alumno.be_in_gym()
+
+persona = Person()
+print(type('persona'))
+
+from abc import ABC, abstractmethod
+
+class Vehicule(ABC):
+    doors = None
+    wheels = None
+
+    @abstractmethod
+    def avanzar(self):
+        pass
+
+class Car(Vehicule):
+    doors = 5
+    wheels = 4
+
+    def avanzar(self):
+        return 'Avanzo en 4 ruedas'
+
+class Bike(Vehicule):
+    door = 0
+    wheels = 2
+
+    def avanzar(self):
+        print(f'Avanzo en {self.wheels} ruedas')
+
+    def __str__(self):
+        return f'Esta es una moto con {self.door} puertas y {self.wheels} ruedas'
+
+
+#vehiculo = Vehicule()
+#vehiculo.avanzar()
+carro = Car()
+print(carro.doors)
+
+bike = Bike()
+bike.avanzar()
+print(bike)
+
+
+class User():
+    password = 'contraseña-encriptada'
+
+    def __init__(self) -> None:
+        self.password = self.__decrypt()
+
+    def __decrypt(self):
+        return 'contraseña-desencriptada'
+
+usuario = User()
+print(usuario.password)
+
+
